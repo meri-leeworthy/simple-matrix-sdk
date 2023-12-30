903b71a5e71cd77b8ce09c8920aaa676c7806dc9
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-31 07:41:42 +1100

    create (empty) timeline class

---
+++ dist/timeline.js
@@ -0,0 +1,9 @@
+"use strict";
+//intention to create a class for events timeline similiar to the matrix-js-sdl
+//create a normalised, structured, validated, deduplicated tree of relations between events
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Timeline = void 0;
+//also goal to have strict typing for events
+class Timeline {
+}
+exports.Timeline = Timeline;
---
+++ src/timeline.ts
@@ -0,0 +1,5 @@
+//intention to create a class for events timeline similiar to the matrix-js-sdl
+//create a normalised, structured, validated, deduplicated tree of relations between events
+
+//also goal to have strict typing for events
+export class Timeline {}
