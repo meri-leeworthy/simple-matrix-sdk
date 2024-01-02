c88f33eb9b76494ab13eb50dc80f0b46efaa4cc3
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-03 10:31:36 +1100

    factor addChunk and addEvent methods

--- dist/timeline.d.ts
+++ dist/timeline.d.ts
@@ -2,4 +2,6 @@ import { ClientEventOutput, Event } from ".";
 export declare class Timeline {
     events: Map<string, Event>;
     constructor(chunk: ClientEventOutput[]);
+    addChunk(chunk: ClientEventOutput[]): void;
+    addEvent(event: ClientEventOutput): void;
 }
