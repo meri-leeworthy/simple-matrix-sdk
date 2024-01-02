bf7a14d2888d10902e8c03271ae31e694b177f4c
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-03 10:31:21 +1100

    factor addChunk and addEvent methods

--- src/timeline.ts
+++ src/timeline.ts
@@ -8,24 +8,31 @@ export class Timeline {
   events: Map<string, Event> = new Map<string, Event>()
 
   constructor(chunk: ClientEventOutput[]) {
-    chunk.forEach(event => {
-      if (
-        is(EventContentSchema, event.content) &&
-        event.content["m.relates_to"] &&
-        event.content["m.relates_to"].rel_type === "m.replace"
-      ) {
-        // set the Event as a replacement on the Event it replaces rather than setting a new Event at the root of the map
-        const id = event.content["m.relates_to"].event_id
-        const eventParent = this.events.get(id)
+    this.addChunk(chunk)
+  }
 
-        if (eventParent) {
-          eventParent.edits.set(event.event_id, new Event(event))
-        } else {
-          this.events.set(event.event_id, new Event(event))
-        }
+  addChunk(chunk: ClientEventOutput[]) {
+    chunk.forEach(event => {
+      this.addEvent(event)
+    })
+  }
+
+  addEvent(event: ClientEventOutput) {
+    if (
+      is(EventContentSchema, event.content) &&
+      event.content["m.relates_to"] &&
+      event.content["m.relates_to"].rel_type === "m.replace"
+    ) {
+      const id = event.content["m.relates_to"].event_id
+      const eventParent = this.events.get(id)
+
+      if (eventParent) {
+        eventParent.edits.set(event.event_id, new Event(event))
       } else {
         this.events.set(event.event_id, new Event(event))
       }
-    })
+    } else {
+      this.events.set(event.event_id, new Event(event))
+    }
   }
 }
